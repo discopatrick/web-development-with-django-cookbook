9bd8491e427bf5a843c9f2283a90b3ec17000fc0
discopatrick <git@discopatrick.me>
2018-02-11 18:11:10 +0000

    Add mptt-enabled models to movies app

--- cookbookproj/movies/models.py
+++ cookbookproj/movies/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from mptt.models import MPTTModel
+from mptt.fields import TreeForeignKey, TreeManyToManyField
+
+from cookbookproj.mixins import CreationModificationDateMixin
+
+
+class Category(MPTTModel, CreationModificationDateMixin):
+    parent = TreeForeignKey('self',
+                            on_delete=models.CASCADE,
+                            blank=True,
+                            null=True)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['tree_id', 'lft']
+        verbose_name_plural = 'categories'
+
+
+class Movie(CreationModificationDateMixin):
+    title = models.CharField(max_length=255)
+    categories = TreeManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'movies'
