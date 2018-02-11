1a5341958461c9ca75ff79b13d5f53db4f9fb2af
discopatrick <git@discopatrick.me>
2018-02-11 18:26:17 +0000

    Use django-mptt-admin for CategoryAdmin

--- cookbookproj/movies/admin.py
+++ cookbookproj/movies/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
+from django_mptt_admin.admin import DjangoMpttAdmin
+
 from .models import (
     Category,
     Movie,
 )
 
 
-admin.site.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    list_display = ['title', 'created', 'modified']
+    list_filter = ['created']
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Movie)
