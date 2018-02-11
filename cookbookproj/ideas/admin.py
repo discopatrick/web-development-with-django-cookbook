f75159befe1c0263ffe39dd4d2a37feeac984e00
discopatrick <git@discopatrick.me>
2018-02-11 14:07:14 +0000

    Create category model as foreign key to Idea

--- cookbookproj/ideas/admin.py
+++ cookbookproj/ideas/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Idea
+from .models import (
+    Category,
+    Idea,
+)
 
 
+admin.site.register(Category)
 admin.site.register(Idea)
--- cookbookproj/ideas/models.py
+++ cookbookproj/ideas/models.py
@@ -3,9 +3,23 @@ from django.db import models
 from .mixins import CreationModificationDateMixin
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Idea(CreationModificationDateMixin, models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 null=True,
+                                 blank=True)
 
     def __str__(self):
         return self.title
